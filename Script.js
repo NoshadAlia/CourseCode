39cf8591a9e32c5f63947f58c7328060185d08f2
NoshadAli <121970724+NoshadAlia@users.noreply.github.com>
2024-09-06 21:12:49 +0500

    Tailwind Css landing website

---
+++ Script.js
@@ -0,0 +1,15 @@
+let x = document.getElementById("Signup");
+let y = document.getElementById("Login");
+let z = document.getElementById("btn");
+
+function loginform(){
+    x.style.left = "-400px";
+    y.style.left = "0px";
+    z.style.left = "160px";
+}
+
+function signupform(){
+    x.style.left = "0px";
+    y.style.left = "400px";
+    z.style.left = "0px";
+}
